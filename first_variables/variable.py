eb39147fac980a83162a0e20f2cc79d046493ae5
gupta143587 <buddepu.gupta@gmail.com>
2025-07-16 20:29:58 +0530

    Varibles in python

---
+++ first_variables/variable.py
@@ -0,0 +1,38 @@
+# name="gupta"
+# age=22
+
+# ti=22
+# print(id(name))
+# print(id(age))
+# print(id(ti))
+
+# # mutable
+# list1 = [1,2,3]
+# list2 = [1,2,3]
+# list3 = [1,2,3]
+# print(id(list1))
+# print(id(list2))
+# print(id(list3))
+
+# print(type(name))
+# print(type(age))
+# print(type(ti))
+
+# x="Yogesh"
+# y="war"
+# z=" Gupta"
+# print(x+y+z)
+
+
+# a=2
+# b=3
+# print(a+b)
+
+# print(x,b)
+
+# e,f,g="Yoges","wara","Gupta"
+# print(e,f,g)
+
+# a=b="gupta"
+# print(a+b)
+
