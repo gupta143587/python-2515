76bdc4b26b61b37bbbe87aabbef94b24bd0ab523
gupta143587 <buddepu.gupta@gmail.com>
2025-07-16 20:51:37 +0530

    Example of variable(cars)

---
+++ first_variables/car_dekho.py
@@ -0,0 +1,37 @@
+#example of Variable
+car_name="Creta"
+arai_Mileage=19.1
+fuel_type="Diesel"
+engine_displacement="1493cc"
+no_of_cylinders=4
+seaitng_capacity=5
+transmission_type="Automatic"
+fuel_tank_capacity=50
+body_type="SUV"
+power_steering=True
+wheel_covers=False
+
+print("Car Name:",car_name)
+print("ARIA iMileage:",arai_Mileage)
+print("Fuel Type:",fuel_type)
+print("Engine Displacement:",engine_displacement)
+print("No. Of Cylinders:",no_of_cylinders)
+print("Seaitng Capacity:",seaitng_capacity)
+print("Transmission Type:",transmission_type)
+print("fuel_tank_capacity:",fuel_tank_capacity)
+print("body_type:",body_type)
+print("power_steering:",power_steering)
+print("wheel_covers:",wheel_covers)
+
+
+print(type(car_name))
+print(type(arai_Mileage))
+print(type(fuel_type))
+print(type(engine_displacement))
+print(type(no_of_cylinders))
+print(type(seaitng_capacity))
+print(type(transmission_type))
+print(type(fuel_tank_capacity))
+print(type(body_type))
+print(type(power_steering))
+print(type(wheel_covers))
