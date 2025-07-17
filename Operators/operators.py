0504c9a2cb737c70ebe0c78c33a604598a66377e
gupta143587 <buddepu.gupta@gmail.com>
2025-07-17 10:05:35 +0530

    operators Examples

---
+++ Operators/operators.py
@@ -0,0 +1,51 @@
+a=3
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+
+n="gupta"
+m="yoges"
+y="YOGES"
+print(n==m)
+print(n>m)
+print(n<m)
+print(y>m)
+
+
+q=2
+e=3
+r=4
+t=1
+rr= q>e and r>t
+print(rr)
+print(not rr)
+
+
+a="Yogeswara Gupta"
+z='z' in a
+l='y' in a
+l1='y' not in a
+print(z)
+print(l)
+print(l1)
+
+
+m1=10
+m2=11
+print(m1 is m2)
+print(m1 is not m2)
+
+
+a=3
+b=5
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(3<<2)
+print(8>>2)
