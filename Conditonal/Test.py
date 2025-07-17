8e623f4865d3c194bbbf3c4839969eeee55377da
gupta143587 <buddepu.gupta@gmail.com>
2025-07-17 19:37:50 +0530

    Some Examples of Conditonal Statements

---
+++ Conditonal/Test.py
@@ -0,0 +1,23 @@
+a=4
+b=5
+if a>b:
+    print(f"{a} is Greater that {b}")
+    print("next statement")
+else:
+    print(f"{a} is Lesser Than {b}")
+    print("next statement")
+
+
+# i=int(input("enter the name:"))
+# if i>=18:
+#     print("Applicable to Vote")
+# else:
+#     print("Not Applicable to Vote")
+    
+
+age=int(input("Enter the Age:"))
+nationality=input("Enter the Nationality:")
+if age>=18 and nationality=="Indian":
+    print("You can Vote")
+else:
+    print("You can't Vote")
